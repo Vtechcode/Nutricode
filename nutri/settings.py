8f0d6f651da9de4e867cb26764e14bdfba055561
Victor Waburi <victorwaburi@gmail.com>
2022-10-01 09:55:46 +0300

    Collectstatic

--- nutri/settings.py
+++ nutri/settings.py
@@ -80,7 +80,10 @@ WSGI_APPLICATION = 'nutri.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
