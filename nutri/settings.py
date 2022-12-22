1bf88991f54d0f91865e211571ff1150a06ceacb
Victor Waburi <victorwaburi@gmail.com>
2022-12-22 23:20:42 +0300

    Changed the secret keys settings

--- nutri/settings.py
+++ nutri/settings.py
@@ -14,6 +14,7 @@ import os
 import django_heroku
 import dj_database_url
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,12 +25,12 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w6re@vvxnzrlptjq41-h@3ww5w%s7^k6w-a3l_q8&38_&4bs%!'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['health-nutricode.herokuapp.com', '127.0.0.1', 'nutricode.azurewebsites.net']
+ALLOWED_HOSTS = [config('ALLOWED_HOST'), '127.0.0.1']
 
 
 # Application definition
